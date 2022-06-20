226099522872f51d04dc8c53ba3b2a1514f27b46
Goanna7007 <45587025+Goanna7007@users.noreply.github.com>
2022-06-20 15:48:14 +1000

    got one branch scrolling

--- PersonalWebsite/src/app/app.component.ts
+++ PersonalWebsite/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, QueryList, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit  {
+  
   title = 'PersonalWebsite';
+
+  @ViewChild('branchpath') pathElem: any;
+
+  @ViewChildren('branchpath') pathElems: QueryList<any> | undefined;
+
+
+
+  ngAfterViewInit(): void {
+    var path : SVGPathElement = this.pathElem.nativeElement;
+    var pathLength = path.getTotalLength();
+
+    path.style.strokeDasharray = pathLength + ' ' + pathLength;
+    path.style.strokeDashoffset = pathLength.toString();
+
+    window.addEventListener('scroll', () => {
+      var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+      var drawLength = pathLength * scrollPercentage;
+      path.style.strokeDashoffset = (pathLength - drawLength).toString();
+    })
+
+  }
+  
+
 }
